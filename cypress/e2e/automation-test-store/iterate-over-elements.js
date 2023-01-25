034278906701e9bf00c10ea7db722b4bc886eb5c
Norbert <dajkas2005@gmail.com>
2023-01-25 21:12:27 +0200

    iterate + add to basket

--- cypress/e2e/automation-test-store/iterate-over-elements.js
+++ cypress/e2e/automation-test-store/iterate-over-elements.js
@@ -8,8 +8,7 @@ describe('Iterate over element', () => {
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
 
         cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
-
-            cy.log("Index:" + index + " : " + $el.text());
+               
             
         })
     })
@@ -17,5 +16,14 @@ describe('Iterate over element', () => {
     it('Add specific product to basket', () => {
         cy.visit('https://automationteststore.com/');
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+        
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+            if($el.text().includes('Curls to straight Shampoo'))  {
+                cy.wrap($el).click();}        
+                              
+            
+        })
+        cy.get('.cart').click(); 
+
     })
 })
