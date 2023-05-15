cf34b03a8ba3165178f062fdb3c3e2c23e1a3b06
Norbert <dajkas2005@gmail.com>
2023-05-15 20:46:58 +0300

    allias-invoke added

--- cypress/e2e/automation-test-store/alies-invoke.js
+++ cypress/e2e/automation-test-store/alies-invoke.js
@@ -13,11 +13,35 @@ describe('Alias and invoke', () => {
             
         })
         
-        it.only('Validate product thumbnail', () => {
-            cy.visit('https://automationteststore.com/');
-            cy.get('.thumbnail').as('productThumbnail');
-            cy.get('@productThumbnail').should('have.length',16);
-            cy.get('@productThumbnail').find('.productcart').invoke('attr','title').should('include','Add to Cart');  
+    it('Validate product thumbnail', () => {
+        cy.visit('https://automationteststore.com/');
+        cy.get('.thumbnail').as('productThumbnail');
+        cy.get('@productThumbnail').should('have.length',16);
+        cy.get('@productThumbnail').find('.productcart').invoke('attr','title').should('include','Add to Cart');  
                 
-            })
-})
+        })
+
+    it.only('Validate product thumbnail', () => {
+        cy.visit('https://automationteststore.com/');
+        // cy.get('.thumbnail').as('productThumbnail');
+        // cy.get('@productThumbnail').find('.oneprice').each(($el, index, $list) => {
+        //     cy.log($el.text());
+        cy.get('.thumbnail').find('.oneprice').invoke('text').as('itemPrice');
+        cy.get('@itemPrice').then($linkText => {
+            var itemPriceTotal = 0;
+            var itemPrice = $linkText.split('$')
+            var i
+            for (i=0; i<itemPrice.length; i++) {
+                cy.log(itemPrice[i]);
+                itemPriceTotal += Number(itemPrice[i]); 
+                              
+            }
+            cy.log(itemPriceTotal);
+        });
+
+            
+                              
+            
+        })
+                    
+        })
